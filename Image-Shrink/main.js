a5e6a7645685c57f823772700760d144b7d71a4d
RabinPhaiju <rabinphaiju15@gmail.com>
2021-05-06 15:24:39 +0545

    isMac or isWin checked

--- Image-Shrink/main.js
+++ Image-Shrink/main.js
@@ -1,5 +1,12 @@
 const { app, BrowserWindow } = require("electron");
 
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+// console.log(process.platform);
+const isMac = process.platform === "darwin" ? true : false;
+const isWin = process.platform === "win32" ? true : false;
+
 let mainWindow;
 
 function createMainWindow() {
@@ -7,8 +14,8 @@ function createMainWindow() {
     title: "Image Shrink",
     width: 800,
     height: 600,
-
     icon: "./assets/icons/Icon_256x256.png",
+    resizable: isDev ? true : false,
   });
   // mainWindow.loadURL("https://twitter.com");
   // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
@@ -16,3 +23,14 @@ function createMainWindow() {
 }
 
 app.on("ready", createMainWindow);
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
