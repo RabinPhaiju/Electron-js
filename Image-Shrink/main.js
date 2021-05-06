91e27e74b85e2a226338cb960720bd2daf99941e
RabinPhaiju <rabinphaiju15@gmail.com>
2021-05-06 15:11:40 +0545

    icon and index.html loaded

--- Image-Shrink/main.js
+++ Image-Shrink/main.js
@@ -1,11 +1,18 @@
 const { app, BrowserWindow } = require("electron");
 
+let mainWindow;
+
 function createMainWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: "Image Shrink",
     width: 800,
     height: 600,
+
+    icon: "./assets/icons/Icon_256x256.png",
   });
+  // mainWindow.loadURL("https://twitter.com");
+  // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.loadFile("./app/index.html");
 }
 
 app.on("ready", createMainWindow);
